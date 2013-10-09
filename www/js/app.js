c4f85d3128a23f796b8be3b9978e9682cd0ec400
R. S. Doiel <rsdoiel@gmail.com>
2013-10-08 21:36:58 -0700

    snapshot

---
+++ www/js/app.js
@@ -0,0 +1,11 @@
+/**
+ * app.js - basic App code goes here.
+ */
+(function(global) {
+    "use strict";
+    // Export as App
+    if (global.App === undefined) {
+       global.App = {};
+    }
+    // Export functions e.g. globalApp.myfunc = myfunc;
+}(this));
